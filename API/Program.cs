eb65a4b51567ddcc3c352c37861c6526181e5453
Christian Ziller <christian.r.ziller@gmail.com>
2025-02-15 13:27:45 -0500

    Added React, Material UI and Axios

--- API/Program.cs
+++ API/Program.cs
@@ -9,10 +9,13 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000", "https://localhost:3000"));
+
 app.MapControllers();
 
 // Everything will be desposed of after this is out of scope
